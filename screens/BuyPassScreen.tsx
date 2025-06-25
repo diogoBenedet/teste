7120ae9eb50de40c90f0c966d961f79c7e0d53f2
diogoBenedet <diogo.benedet@unochapeco.edu.br>
2025-06-25 01:11:31 -0300

    terceito commit

--- screens/BuyPassScreen.tsx
+++ screens/BuyPassScreen.tsx
@@ -54,7 +54,7 @@ export default function BuyPassScreen() {
 
       {/* Botão Formas de Pagamento */}
       <TouchableOpacity style={styles.paymentButton}>
-        <Text style={styles.paymentText}>Formas de pagamento</Text>
+        <Text style={styles.paymentText}>Extrato detahado</Text>
         <Ionicons name="chevron-down" size={20} color="black" />
       </TouchableOpacity>
 
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -32,12 +32,12 @@ export default function HomeScreen() {
 
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('BusTracking')}>
         <MaterialIcons name="directions-bus" size={24} color="black" />
-        <Text style={styles.buttonText}>Comprar Bilhete</Text>
+        <Text style={styles.buttonText}>Fazer Recarga</Text>
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('BuyPass')}>
         <FontAwesome name="credit-card" size={22} color="black" />
-        <Text style={styles.buttonText}>Acompanhar Saldo</Text>
+        <Text style={styles.buttonText}>Extrato</Text>
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('PlanRoute')}>
